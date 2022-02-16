5faa0af52526a2866b8cdf30f9d17425cc79e184
Hristiyan Genchev <genchev99@users.noreply.github.com>
2022-02-16 11:52:26 +0200

    Export questions as csv

--- src/utils.php
+++ src/utils.php
@@ -9,3 +9,28 @@ function test_input($input): string {
     $input = htmlspecialchars($input);
     return stripslashes($input);
 }
+
+function array_to_csv_download($array, $filename = "export.csv", $delimiter=";") {
+    header('Content-Type: application/csv');
+    header('Content-Disposition: attachment; filename="'.$filename.'";');
+
+    // open the "output" stream
+    // see http://www.php.net/manual/en/wrappers.php.php#refsect2-wrappers.php-unknown-unknown-unknown-descriptioq
+    $f = fopen('php://output', 'w');
+
+    foreach ($array as $line) {
+        fputcsv($f, $line, $delimiter);
+    }
+}
+
+function first_index_of_arr_of_objects($arr, $key, $value): int {
+    $index = 0;
+    foreach ($arr as $obj) {
+        if (isset($obj[$key]) && $obj[$key] == $value) {
+            return $index;
+        }
+        $index += 1;
+    }
+
+    return -1;
+}
